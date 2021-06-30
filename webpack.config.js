a1821517a27972174da7e65f97409d48d79b9b24
Nikolay Komarov <kny.komarov@gmail.com>
2021-06-30 22:13:34 +0300

    добавит настройки webpack для сборки проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
-  mode: 'development',
+  mode: NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  watch: NODE_ENV === 'development',
   watchOptions: {
     ignored: /node_modules/,
     poll: 1000,
@@ -48,10 +50,11 @@ module.exports = {
     }),
   ],
   devServer: {
-    port: 3000,
+    port: 5000,
     overlay: true,
     open: true,
     hot: true,
     historyApiFallback: true,
   },
+  devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
